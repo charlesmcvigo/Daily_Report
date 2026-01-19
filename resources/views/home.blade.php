3ca07111e6a108fe473395cf1d0492191855e044
Charles Mc <charlesvigo8@gmail.com>
2026-01-20 04:06:45 +0800

    home page added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+    <h1>Hello Home Page</h1>
+
+
+    <a href="{{ route('dashboard') }}" class="font-"> Go to Dashboard
+    </a>
+</body>
+
+</html>
